9ffcb74e8dbb9884951b70fb7fa9a53f768ccbd4
Ronan Doherty <ronan.odochartaigh@gmail.com>
2019-01-01 23:17:28 +0000

    Separated user and password storage logic

--- src/Data/UserRepository.cs
+++ src/Data/UserRepository.cs
@@ -1,10 +1,9 @@
 using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Transactions;
 using Microsoft.Data.Sqlite;
-using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Options;
 using Dapper;
 using Schematic.Core;
@@ -27,36 +26,28 @@ namespace Schematic.BaseInfrastructure.Sqlite
             _connectionString = _settings.CurrentValue.DataStore.ConnectionString;
         }
 
-        public async Task<int> CreateAsync(User resource, string token, int userID)
+        public async Task<int> CreateAsync(User resource, int userID)
         {
-            using (var db = new SqliteConnection(_connectionString))
+            using (var transaction = new TransactionScope())
             {
-                db.Open();
-
-                using (var transaction = db.BeginTransaction())
+                using (var db = new SqliteConnection(_connectionString))
                 {
-                    var dateCreated = DateTime.UtcNow;
-
                     var createUser = await db.QueryAsync<int>(@"INSERT INTO Users (Forenames, Surnames, Email, DateCreated, CreatedBy)
                             VALUES (@Forenames, @Surnames, @Email, @DateCreated, @CreatedBy);" +
                             "SELECT last_insert_rowid();", 
                         new { Forenames = resource.Forenames, Surnames = resource.Surnames, Email = resource.Email, 
-                            PassHash = resource.PassHash, DateCreated = dateCreated, CreatedBy = userID });
+                            PassHash = resource.PassHash, DateCreated = DateTime.UtcNow, CreatedBy = userID });
                             
-                    int userId = createUser.FirstOrDefault();
-
-                    await db.ExecuteAsync(@"INSERT INTO UsersPasswordToken (UserID, Email, Token, DateCreated) 
-                            VALUES (@UserID, @Email, @Token, @DateCreated)",
-                        new { UserID = userId, Email = resource.Email, Token = token, DateCreated = dateCreated });
+                    var userId = createUser.FirstOrDefault();
 
                     foreach (var role in resource.Roles.Where(r => r.HasRole))
                     {
                         await db.ExecuteAsync(@"INSERT INTO UsersUserRole (UserID, RoleID, DateCreated, CreatedBy) 
                                 VALUES (@UserID, @RoleID, @DateCreated, @CreatedBy)", 
-                            new { UserID = userId, RoleID = role.ID, DateCreated = dateCreated, CreatedBy = userID });
+                            new { UserID = userId, RoleID = role.ID, DateCreated = DateTime.UtcNow, CreatedBy = userID });
                     }
 
-                    transaction.Commit();
+                    transaction.Complete();
                     return userId;
                 }
             }
@@ -65,7 +56,7 @@ namespace Schematic.BaseInfrastructure.Sqlite
         public async Task<User> ReadAsync(UserSpecification userSpecification)
         {
             var builder = new SqlBuilder();
-            var template = builder.AddTemplate(@"SELECT * FROM Users /**where**/;");
+            var template = builder.AddTemplate(@"SELECT * FROM Users /**where**/ LIMIT 1;");
 
             if (userSpecification.Email.HasValue())
             {
@@ -86,25 +77,27 @@ namespace Schematic.BaseInfrastructure.Sqlite
                 var readUser = await db.QueryAsync<User>(template.RawSql, template.Parameters);
                 var user = readUser.FirstOrDefault();
 
-                if (user != null)
+                if (user is null)
                 {
-                    using (var multi = await db.QueryMultipleAsync(roleSql, new { ID = user.ID }))
+                    return null;
+                }
+
+                using (var multi = await db.QueryMultipleAsync(roleSql, new { ID = user.ID }))
+                {
+                    var roles = multi.Read<UserRole>().ToList();
+                    var userRoleIDs = multi.Read<int>();
+
+                    foreach (var role in roles)
                     {
-                        var roles = multi.Read<UserRole>().ToList();
-                        var userRoleIDs = multi.Read<int>();
-
-                        foreach (var role in roles)
-                        {
-                            role.HasRole = false;
-                        }
-
-                        foreach (int roleID in userRoleIDs)
-                        {
-                            roles.Find(r => r.ID == roleID).HasRole = true;
-                        }
-
-                        user.Roles = roles;
+                        role.HasRole = false;
                     }
+
+                    foreach (int roleID in userRoleIDs)
+                    {
+                        roles.Find(r => r.ID == roleID).HasRole = true;
+                    }
+
+                    user.Roles = roles;
                 }
 
                 return user;
@@ -114,16 +107,15 @@ namespace Schematic.BaseInfrastructure.Sqlite
         public async Task<int> UpdateAsync(User resource, int userID)
         {
             const string sql = @"UPDATE Users 
-                SET Forenames = @Forenames, Surnames = @Surnames, Email = @Email, PassHash = @PassHash WHERE ID = @ID;
+                SET Forenames = @Forenames, Surnames = @Surnames, Email = @Email, PassHash = @PassHash 
+                WHERE ID = @ID;
                 DELETE FROM UsersUserRole WHERE UserID = @ID;";
 
-            using (var db = new SqliteConnection(_connectionString))
+            using (var transaction = new TransactionScope())
             {
-                db.Open();
-
-                using (var transaction = db.BeginTransaction())
+                using (var db = new SqliteConnection(_connectionString))
                 {
-                    int updatedUser = await db.ExecuteAsync(sql, new { ID = resource.ID, Forenames = resource.Forenames, 
+                    var updatedUser = await db.ExecuteAsync(sql, new { ID = resource.ID, Forenames = resource.Forenames, 
                             Surnames = resource.Surnames, Email = resource.Email, PassHash = resource.PassHash });
 
                     foreach (var role in resource.Roles.Where(r => r.HasRole))
@@ -133,7 +125,7 @@ namespace Schematic.BaseInfrastructure.Sqlite
                             new { UserID = resource.ID, RoleID = role.ID, DateCreated = DateTime.UtcNow, CreatedBy = userID });
                     }
 
-                    transaction.Commit();
+                    transaction.Complete();
                     return updatedUser;
                 }
             }
@@ -168,59 +160,6 @@ namespace Schematic.BaseInfrastructure.Sqlite
                 var users = await db.QueryAsync<User>(template.RawSql, 
                     new { Query = filter.SearchQuery + "%" });
                 return users.ToList();
-            }
-        }
-
-        public async Task<bool> SaveTokenAsync(User resource, string token)
-        {
-            const string sql = @"INSERT INTO UsersPasswordToken (UserID, Email, Token, NotificationsSent, DateCreated) 
-                VALUES (@UserID, @Email, @Token, @NotificationsSent, @DateCreated)";
-
-            using (var db = new SqliteConnection(_connectionString))
-            {
-                var tokenSaved = await db.ExecuteAsync(sql, new { UserID = resource.ID, Email = resource.Email, Token = token, 
-                    NotificationsSent = 0, DateCreated = DateTime.UtcNow });
-                return (tokenSaved > 0) ? true : false; 
-            }
-        }
-
-        public async Task<TokenVerificationResult> ValidateTokenAsync(string email, string token)
-        {
-            const string sql = @"SELECT ID, DateCreated FROM UsersPasswordToken 
-                WHERE Email = @Email AND Token = @Token LIMIT 1";
-
-            using (var db = new SqliteConnection(_connectionString))
-            {
-                var result = await db.QueryAsync(sql, new { Email = email, Token = token });
-                var tokenResult = result.FirstOrDefault();
-
-                if (tokenResult == null || tokenResult.Count() == 0)
-                {
-                    return TokenVerificationResult.Invalid;
-                }
-
-                var timeCreated = DateTime.Parse(tokenResult.DateCreated);
-                var timeLimit = _settings.CurrentValue.SetPasswordTimeLimitHours;
-
-                if (timeCreated < DateTime.UtcNow.Subtract(TimeSpan.FromHours(timeLimit)))
-                {
-                    return TokenVerificationResult.Expired;
-                }
-                else
-                {
-                    return TokenVerificationResult.Success;
-                }
-            }
-        }
-
-        public async Task<bool> SetPasswordAsync(User resource, string passHash)
-        {
-            const string sql = @"UPDATE Users SET PassHash = @PassHash WHERE ID = @ID";
-
-            using (var db = new SqliteConnection(_connectionString))
-            {
-                var passwordSet = await db.ExecuteAsync(sql, new { ID = resource.ID, PassHash = passHash });
-                return (passwordSet > 0) ? true : false; 
             }
         }
     }
